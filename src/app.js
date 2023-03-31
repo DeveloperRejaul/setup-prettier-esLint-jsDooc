e79023a3205ea0bb907994250e99b5f646ffc57e
rejaul <devrejaul.official@gmail.com>
2023-03-31 19:52:09 +0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const name = 'rejaul';
+
+/**
+ *
+ * @param {string} params string value
+ * @returns {string} return params value
+ */
+function hello(params) {
+  return params;
+}
+
+hello(name);
